0800308828fd4c15ab60356df72ae6fbcffba4f5
Loayalmofadl <112723120+Loayalmofadl@users.noreply.github.com>
2025-02-08 00:13:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const loadSavedChatHistory = () => {
         const userMessageHtml = `
 
             <div class="message__content">
-                <img class="message__avatar" src="assets/profile.png" alt="User avatar">
+                <img class="message__avatar" src="https://i.suar.me/jM2z1/m" alt="User avatar">
                <p class="message__text">${conversation.userMessage}</p>
             </div>
         
@@ -45,7 +45,7 @@ const loadSavedChatHistory = () => {
         const responseHtml = `
         
            <div class="message__content">
-                <img class="message__avatar" src="assets/gemini.svg" alt="Gemini avatar">
+                <img class="message__avatar" src="https://i.suar.me/8G03M/m" alt="Gemini avatar">
                 <p class="message__text"></p>
                 <div class="message__loading-indicator hide">
                     <div class="message__loading-bar"></div>
