5d057b95dbb3e549309996c07172871686e182a9
Loayalmofadl <112723120+Loayalmofadl@users.noreply.github.com>
2025-02-08 11:31:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const loadSavedChatHistory = () => {
         const userMessageHtml = `
 
             <div class="message__content">
-                <img class="message__avatar" src="https://i.suar.me/jM2z1/m" alt="User avatar">
+                <img class="message__avatar" src="https://i.suar.me/KoOgz/m" alt="User avatar">
                <p class="message__text">${conversation.userMessage}</p>
             </div>
         
@@ -45,7 +45,7 @@ const loadSavedChatHistory = () => {
         const responseHtml = `
         
            <div class="message__content">
-                <img class="message__avatar" src="https://github.com/Loayalmofadl/Quantum-Chat/blob/main/ezgif-2e2aba9780367-ezgif.com-gif-maker%20(1).gif" alt="Gemini avatar">
+                <img class="message__avatar" src="https://i.suar.me/BqEWg/m" alt="Gemini avatar">
                 <p class="message__text"></p>
                 <div class="message__loading-indicator hide">
                     <div class="message__loading-bar"></div>
@@ -182,7 +182,7 @@ const displayLoadingAnimation = () => {
     const loadingHtml = `
 
         <div class="message__content">
-            <img class="message__avatar" src="https://github.com/Loayalmofadl/Quantum-Chat/blob/main/ezgif-2e2aba9780367-ezgif.com-gif-maker%20(1).gif" alt="Gemini avatar">
+            <img class="message__avatar" src="https://i.suar.me/BqEWg/m" alt="Gemini avatar">
             <p class="message__text"></p>
             <div class="message__loading-indicator">
                 <div class="message__loading-bar"></div>
@@ -219,7 +219,7 @@ const handleOutgoingMessage = () => {
     const outgoingMessageHtml = `
     
         <div class="message__content">
-            <img class="message__avatar" src="https://i.suar.me/jM2z1/m" alt="User avatar">
+            <img class="message__avatar" src="https://i.suar.me/KoOgz/m" alt="User avatar">
             <p class="message__text"></p>
         </div>
 
